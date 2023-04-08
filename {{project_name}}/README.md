3a14052ec2339bde2ae2e78006a78a2550d19bb6
Ashwin Vishnu <dev@fluid.quest>
2023-04-09 01:11:53 +0200

    Fix readme and add test dependencies

--- {{project_name}}/README.md
+++
@@ -1,6 +0,0 @@
-Orphan:
-
-# Structure of a Snek5000 solver
-
-This is the example given in
-[our packaging tutorial](https://snek5000.readthedocs.io/en/latest/packaging.html).
--- {{project_name}}/tests/test_solver.py
+++ {{project_name}}/tests/test_solver.py
@@ -1,7 +1,5 @@
 import pytest
 
-from snek5000 import load_for_restart
-
 
 def test_init(sim):
     print(sim.info_solver)
@@ -15,20 +13,3 @@ def test_mesh(sim):
 def test_compile(sim):
     assert sim.make.exec("compile")
     assert sim.make.exec("run", dryrun=True)
-
-
-@pytest.mark.slow
-def test_make_run(sim):
-    # Run in foreground
-    assert sim.make.exec("run_fg", nproc=2)
-
-    # test outputs
-    print(sim.output.print_stdout.path_file)
-    df = sim.output.print_stdout.load()
-
-    # number of time steps executed, see conftest.py
-    assert df.dt.size == sim.params.nek.general.num_steps
-
-    # check if simulation can be restarted
-    # TODO: try restart with modified params
-    _ = load_for_restart(sim.path_run, use_start_from=-1)
